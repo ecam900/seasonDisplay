1fb889e4847e5c8adda6bed1a627148ad8021fe7
Eric Crescioni <ecam900@gmail.com>
2019-01-24 20:43:49 -0400

    Update readme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  // This abbreviates the constructor method, thanks to Babel.
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message  })
+    );
+    console.log('My component was rendered to the screen');
+  }
+
+  componentDidUpdate(){
+    console.log('My component was updated - it rerendered!');
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat= {this.state.lat} />
+    }
+
+    return <Spinner message="Please accept location request!"  />; 
+  }
+
+  render() {
+    return (
+      // This class does nothing, there's no CSS for it. It's just an example.
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
